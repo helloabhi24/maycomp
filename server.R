e60823b444ab6298f82271d55989f010e4527a36
Abhishek Kumar <40750418+helloabhi24@users.noreply.github.com>
2018-08-19 23:56:26 +0530

    Update server.R

--- server.R
+++ server.R
@@ -14,7 +14,7 @@ shinyServer(function(input,output){
   })
   
   output$plot1<-renderPlotly({
-    plot_ly(data11,x=~Year,y=~g1(),type='bar',name='max'
+    plot_ly(x=~Year,y=~g1(),type='bar',name='max'
     )%>%add_trace(y=~g2(),name="min"
     )%>%add_trace(y=~g3(),name="mean"
     )%>%add_trace(y=~g4(),name='Median'
@@ -36,11 +36,26 @@ shinyServer(function(input,output){
   output$plot9<-renderPlotly({q3})
   output$plot10<-renderPlotly({q14})
   output$plot11<-renderPlotly({p15})
+  output$plot12<-renderPlotly({plo})
+  output$plot13<-renderPlotly({plo1})
+  output$plot14<-renderPlotly({plo11})
   
   output$max1<-renderValueBox({
     valueBox(
       value = g1(),
       subtitle = "Max of 2013",icon("arrow-up")
+    )
+  })
+  output$max11<-renderValueBox({
+    valueBox(
+      value = g1()[2],
+      subtitle = "Max of 2014",icon("arrow-up")
+    )
+  })
+  output$max111<-renderValueBox({
+    valueBox(
+      value = g1()[3],
+      subtitle = "Max of 2015",icon("arrow-up")
     )
   })
   output$min1<-renderValueBox({
@@ -49,10 +64,34 @@ shinyServer(function(input,output){
       subtitle = "Min of 2013",icon("arrow-down")
     )
   })
+  output$min11<-renderValueBox({
+    valueBox(
+      value = g2()[2],
+      subtitle = "Min of 2014",icon("arrow-down")
+    )
+  })
+  output$min111<-renderValueBox({
+    valueBox(
+      value = g2()[3],
+      subtitle = "Min of 2015",icon("arrow-down")
+    )
+  })
   output$mean1<-renderValueBox({
     valueBox(
       value = round(g3(),2),
       subtitle = "Mean of 2013",icon("angle-double-right")
+    )
+  })
+  output$mean11<-renderValueBox({
+    valueBox(
+      value = round(g3()[2],2),
+      subtitle = "Mean of 2014",icon("angle-double-right")
+    )
+  })
+  output$mean111<-renderValueBox({
+    valueBox(
+      value = round(g3()[2],2),
+      subtitle = "Mean of 2015",icon("angle-double-right")
     )
   })
   
